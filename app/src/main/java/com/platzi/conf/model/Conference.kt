52287faaa20e45ca1e82f983228f0daab51708cd
Edorta Rodriguez <burdaich@gmail.com>
2020-12-06 23:42:42 +0100

    Video 30 --> Connecting with firebase and adding data to cloudStorage

--- app/src/main/java/com/platzi/conf/model/Conference.kt
+++ app/src/main/java/com/platzi/conf/model/Conference.kt
@@ -2,10 +2,11 @@ package com.platzi.conf.model
 
 import java.util.*
 
-class Conference {
+class Conference() {
     lateinit var title: String
     lateinit var description: String
     lateinit var topic: String
     lateinit var dateTime: Date
-
+    lateinit var tag: String
+    lateinit var speaker: String
 }
--- app/src/main/java/com/platzi/conf/model/Speaker.kt
+++ app/src/main/java/com/platzi/conf/model/Speaker.kt
@@ -1,9 +1,11 @@
 package com.platzi.conf.model
 
 class Speaker {
-    val name = ""
-    val jobtitle = ""
-    val workplace = ""
-    val twitter = ""
-    val image = ""
+    var name = ""
+    var jobtitle = ""
+    var workplace = ""
+    var twitter = ""
+    var biography = ""
+    var image = ""
+    var category = 0;
 }
