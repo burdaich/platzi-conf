bf135d5a65b2a4ece80718c4af72b6406b23ced1
Edorta <edorta.r@gistek-s.com>
2020-12-17 08:41:00 +0100

    Video 38 --> Opening detail dialog fragment (Schedule)

--- app/src/main/java/com/platzi/conf/view/ui/fragments/ScheduleDetailDialogFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/ScheduleDetailDialogFragment.kt
@@ -1,11 +1,17 @@
 package com.platzi.conf.view.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.platzi.conf.R
+import com.platzi.conf.model.Conference
+import kotlinx.android.synthetic.main.fragment_schedule_detail_dialog.*
+import java.text.SimpleDateFormat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,17 +23,38 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ScheduleDetailDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ScheduleDetailDialogFragment : Fragment() {
+class ScheduleDetailDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setStyle(STYLE_NORMAL, R.style.fullscreenDialogStyle)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolbarConference.navigationIcon =
+            ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarConference.setTitleTextColor(Color.WHITE)
+        toolbarConference.setNavigationOnClickListener { dismiss() }
+
+        val conference = arguments?.getSerializable("conference") as Conference
+        toolbarConference.title = conference.title
+        tvItemScheduleTitle.text = conference.title
+
+        val pattern = "dd/MM/yyyy hh:mm: a"
+        val simpleDF = SimpleDateFormat(pattern)
+        val date = simpleDF.format(conference.dateTime)
+
+
+        tvDetailConferenceHour.text = date
+        tvDetailConferenceSpeaker.text = conference.speaker
+        tvDetailConferenceTag.text = conference.tag
+        tvDetailConferenceDescription.text = conference.description
+
     }
 
     override fun onCreateView(
@@ -38,23 +65,11 @@ class ScheduleDetailDialogFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_schedule_detail_dialog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ScheduleDetailDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ScheduleDetailDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
     }
 }
