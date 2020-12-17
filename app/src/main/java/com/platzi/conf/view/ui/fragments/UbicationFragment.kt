5086c9e9f91e1c5edb42232f24e6a61616f65629
Edorta <edorta.r@gistek-s.com>
2020-12-17 12:35:43 +0100

    Video 41 --> Styling map and ubication detail.

--- app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
@@ -8,14 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.platzi.conf.R
 import com.platzi.conf.model.Ubication
 
@@ -29,7 +27,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [UbicationFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class UbicationFragment : Fragment(), OnMapReadyCallback {
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -69,5 +67,12 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
         markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
 
         googleMap?.addMarker(markerOptions)
+        googleMap?.setOnMarkerClickListener(this)
+        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.custom_map))
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailFragmentDialog)
+        return true
     }
 }
